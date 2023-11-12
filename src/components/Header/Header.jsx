ded5f7f82718a21f19337d445ee406fdd4633415
armanphero <017911sksaifurrahman@gmail.com>
2023-11-12 01:10:08 -0800

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Button from '../Button/Button';
 const Header = () => {
     return (
         <div style={{ margin: '20px 0' }} className='text-center'>
-            <h1 className='text-3xl font-bold text-center mb-3'>AI Universe Hub</h1>
+            <h1 className='text-3xl font-bold text-center mb-3'>AI Universe Hub bd</h1>
         </div>
     );
 };
